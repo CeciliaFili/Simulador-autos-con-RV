d48fc82c4c94578e4e59c6b49fe850de3357c561
Waaflee <agustinwafle@gmail.com>
2020-10-10 22:08:12 -0300

    added Driving/"

---
+++ Scenes/Driving/Driving.gd
@@ -0,0 +1,18 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var car = Singleton.Car.instance()
+	print(car)
+	$CarPoint.add_child(car)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
